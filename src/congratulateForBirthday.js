ea0c33ee1a98cdf0374ce8de00c66afcb4f6c911
Georg Becker <Georg.Becker@gmx.at>
2021-10-04 15:40:58 +0200

    Change script to use mentions

--- src/congratulateForBirthday.js
+++ src/congratulateForBirthday.js
@@ -18,10 +18,7 @@ client.once("ready", async() => {
             for(let uid of (Object.keys(birthdayConfig[guild]["birthdays"]))){
                 let bday = new Date(birthdayConfig[guild]["birthdays"][uid]);
                 if (today.getDate() == bday.getDate() && today.getMonth() == bday.getMonth()){
-                    const guildObj = client.guilds.cache.get(guild);
-                    const member = guildObj.members.cache.get(uid)
-                    const nickname = member.displayName;
-                    channel.send(`Hey ${nickname} it's your birthday! We wish you all the best!`)
+                    channel.send(`Hey <@${uid}> it's your birthday! We wish you all the best!`)
                         .then(message => console.log(`Sent message: ${message.content}`))
                         .catch(console.error);
                 }
