456da18258f7cdc642e1c6f42ed1476d62701ed0
Georg Becker <Georg.Becker@gmx.at>
2021-10-05 10:54:53 +0200

    Change system to think about respone coming in multiple chunks

--- src/dailyCalendarUpdate.js
+++ src/dailyCalendarUpdate.js
@@ -2,28 +2,44 @@ const https = require('https');
 
 let groupACalender = null;
 let groupBCalender = null;
+let groupACalenderDone = false;
+let groupBCalenderDone = false;
 
 const groupARequest = https.get("https://cis.fhstp.ac.at/addons/STPCore/cis/meincis/cal.php?tiny=stp60c9a6d405be7", function(response) {
     response.on('data', function(body){
-        groupACalender = body;
+        if (groupACalender == null) {
+            groupACalender = body;
+        } else {
+            groupACalender += body;
+        }
+    });
+    response.on('end', function() {
+        groupACalenderDone = true;
         printCalenders();
     });
 });
 
 const groupBRequest = https.get("https://cis.fhstp.ac.at/addons/STPCore/cis/meincis/cal.php?tiny=stp60c9a6d8a4207", function(response) {
     response.on('data', function(body){
-        groupBCalender = body;
+        if (groupBCalender == null) {
+            groupBCalender = body;
+        } else {
+            groupBCalender += body;
+        }
+    });
+    response.on('end', function() {
+        groupBCalenderDone = true;
         printCalenders();
     });
 });
 
 function bothCalendersLoaded() {
-    return groupACalender != null && groupBCalender != null;
+    return groupACalenderDone && groupBCalenderDone;
 }
 
 function printCalenders() {
     if (bothCalendersLoaded()) {
-        console.log("GROUP A: " + groupACalender);
-        console.log("GROUP B: " + groupBCalender);
+        console.log(groupACalender);
+        console.log(groupBCalender);
     }
-}
+};
